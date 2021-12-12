c8ca290740812da6976bee985468538a188711cd
Anass Ouazri <78270875+Aouazri@users.noreply.github.com>
2021-12-12 18:16:41 +0100

    Update and rename main (4).py to main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def calculate_score(list):
   score= sum(list)
   if score==21 and len(list) == 2: 
     return 0
+  #Handling the Ace case, if the score is above 21 we consider the Ace(11) as 1. 
   if 11 in list and score>21:
     list.remove(11)
     list.append(1)
@@ -49,8 +50,7 @@ def play():
   user_cards = []
   computer_cards= []
   for i in range(2): 
-    card_choice= random.choice(cards)
-    user_cards.append(card_choice)
+    deal_card(user_cards)
   #user_score = sum(user_cards)
   user_score = calculate_score(user_cards)
   
@@ -80,7 +80,7 @@ def play():
   ## while computer score under 17 we deal another card ##
   while computer_score<17: 
     deal_card(computer_cards)
-    computer_score= sum(computer_cards)
+    computer_score= calculate_score(computer_cards)
   ## ************************************************** ##
   print(f"   Your cards: {user_cards}, final score: {user_score}")
   print(f"   computer cards {computer_cards}, final score: {computer_score}")
@@ -90,20 +90,20 @@ def play():
   ## *******************  BLACKJACK ************* ## 
   def compare_score(computer_score, user_score):
     if computer_score == 0 :
-      print("You lost, your opponent has a BLACKJACK!! \U0001F62D")
+      print("You lost, your opponent has a BLACKJACK!! \U0001F62D \n ")
     elif user_score == 0 :
-      print("You WIN with a BLACKJACK!! \U0001F929")
+      print("You WIN with a BLACKJACK!! \U0001F929 \n ")
       ## ************************************************** ##
     elif user_score>21:
-      print("You went over, you lose! \U0001F622 ")
+      print("You went over, you lose! \U0001F622 \n ")
     elif computer_score>21:
-      print("Opponent went over. You win! \U0001f600")
+      print("Opponent went over. You win! \U0001f600 \n ")
     elif user_score< computer_score and computer_score<=21: 
-      print("You lose \U0001F622")
+      print("You lose \U0001F622 \n ")
     elif user_score == computer_score:
-      print("It's a draw \U0001F91D")
+      print("It's a draw \U0001F91D \n ")
     else: 
-      print('You win! \U0001f600')
+      print('You win! \U0001f600 \n ')
   compare_score(computer_score, user_score)
   ## *************************************************************** ##
   should_continue = input("Do you want to play a game of Blackjack? Type 'y' or 'n': \n ").lower()
@@ -119,45 +119,4 @@ if play_game =='y':
 
 
 
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 
